f460ce7ebd37ad2b6f04438e67c90afb2e6c3ca0
workArcela <arcela34@gmail.com>
2022-08-01 18:08:13 -0400

    Initial commit

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,12 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import path from "path";
+import { promises as fs } from "fs";
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req, res) {
+  //Find the absolute path of the json directory
+  const jsonDirectory = path.join(process.cwd(), "json");
+  // Read the json data file data.json
+  const fileContents = await fs.readFile(jsonDirectory + "/data.json", "utf8");
+  // const fileContents = await "/data.json";
+  //Return the content of the data file in json format
+  res.status(200).json(fileContents);
 }
